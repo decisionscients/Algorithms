ccccc295673d1347e76f653e7d887dc3080d707f
John James <jjames@decisionscients.com>
2020-02-13 18:12:54 -0500

    QuickSort

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -6,19 +6,26 @@
 #include<iostream>
 #include<stdio.h> 
 #include<stdlib.h>
+#include<string>
 #include<vector>
 using namespace std;
 
-std::vector<long int> Swap(std::vector<long int>& arr,
+void PrintVec(std::vector<long int>& arr, long int left, long int right, string msg) {
+    std::cout << msg;
+    for (unsigned int i = left; i < right+1; i++)
+        std::cout << arr.at(i) << ' ';
+}
+
+void Swap(std::vector<long int>& arr,
     long int idx1, long int idx2) {
     int temp = arr[idx1];
     arr[idx1] = arr[idx2];
     arr[idx2] = temp;
-    return arr;
+    return;
 }
 
 // Return median of value an array
-std::vector<long int> SetMedian(std::vector<long int>& arr,
+void SetMedian(std::vector<long int>& arr,
     long int left, long int right) {
 
     long int mid = 0;
@@ -29,51 +36,60 @@ std::vector<long int> SetMedian(std::vector<long int>& arr,
     // Gets middle value based upon odd or even length
     length = right + 1 - left;
     if (length % 2 == 0)
-        mid = (length / 2) - 1;
+        mid = left + ((length / 2) - 1);
     else
-        mid = length / long int(2);
+        mid = left + (length / long int(2));
 
       
     // Create array containing left, right, and median
-    std::array<long int, n> lrm = { arr[left], arr[mid], arr[right] };
+    std::array<long int, n> lmr = { arr[left], arr[mid], arr[right] };
 
+    /* Print sorted 3 medians
+    std::cout << "  \nLeft : " << lmr[0];    
+    std::cout << "  Right : " << lmr[2];
+    std::cout << "  Middle : " << lmr[1];
+    */
+    
     // Sort the array
-    std::sort(lrm.begin(), lrm.end());
-        
+    std::sort(lmr.begin(), lmr.end());
+    
+    /* Print median value
+    std::cout << "  Median: " << lmr[1];
+    */    
+    
     // Evaluate whether a swap is needed based upon the index of medians array
-    if (lrm[1] == arr[mid])
-        arr = Swap(arr, left, mid);
-    else if (lrm[1] == arr[right])
-        arr = Swap(arr, left, right);
-    return arr;
+    if (lmr[1] == arr[mid])
+        Swap(arr, left, mid);
+    else if (lmr[1] == arr[right])
+        Swap(arr, left, right);
+    return;
 }
 
 // Returns the pivot for a given pivot rule
-std::vector<long int> SetPivot(std::vector<long int>& arr,
+void SetPivot(std::vector<long int>& arr,
     long int left, long int right,
     char pivot_rule) {
     
     if (pivot_rule == 'r')
-        arr = Swap(arr, left, right);
+        Swap(arr, left, right);
     else if (pivot_rule == 'm')
-        arr = SetMedian(arr, left, right);
-    return arr;
+        SetMedian(arr, left, right);
+    return;
 }
 
 
 long int Partition(std::vector<long int>& arr,
     long int left, long int right,
     char pivot_rule) {
-    
-    arr = SetPivot(arr, left, right, pivot_rule);
+
     long int p = arr[left];
     long int i = left + 1;
     for (long int j = left + 1; j <= right; j++)
         if (arr[j] < p) {
-            arr = Swap(arr, j, i);
+            Swap(arr, j, i);
             i = i + 1;
         }
-    arr = Swap(arr, left, i - 1);
+    Swap(arr, left, i - 1);
     return i - 1;
 }
 
@@ -83,12 +99,19 @@ void QuickSort(std::vector<long int>& arr,
     
     long int j = 0;
     long int m = right - left + 1;
+    string msg;
 
     if (left >= right)
         return;
-    j = Partition(arr, left, right, pivot_rule); // partition & set pivot first
 
+    //msg = "\n\nInput array: ";
+    //PrintVec(arr, left, right, msg);
+    SetPivot(arr, left, right, pivot_rule);
+
+    j = Partition(arr, left, right, pivot_rule); // partition & set pivot first
+    
     *num_comparisons = *num_comparisons + m - 1;
+    //std::cout << " Count = " << m-1;
     
     QuickSort(arr, left, j - 1, pivot_rule, num_comparisons);        // recurse on first part.
     QuickSort(arr, j + 1, right, pivot_rule, num_comparisons);       // recurse on second part.    
@@ -134,17 +157,10 @@ int main(int argc, char* argv[])
             long int m = std::size(arr);            
             long int num_comparisons = 0;
 
-            std::cout << "\nSize of input: " << m;
-            std::cout << "\nUnsorted Array: ";
-            for (auto i = arr.begin(); i != arr.end(); ++i)
-                cout << *i << " ";
-            
-            QuickSort(arr, 0, (m - 1), 'r', &num_comparisons);
-            
-            std::cout << "\n\nNumber of Comparisons: " << num_comparisons;
-            std::cout << "\nSorted Array: ";
-            for (auto i = arr.begin(); i != arr.end(); ++i)
-                cout << *i << " ";
+            std::cout << "\nSize of input: " << m;            
+            QuickSort(arr, 0, (m - 1), 'm', &num_comparisons);            
+            std::cout << "\n\nNumber of comparisons: " << num_comparisons;
+
             return 0;
         }
         else
